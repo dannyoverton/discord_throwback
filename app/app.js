c216468d8fff6792c499c4d588476015f7214521
dannyoverton <dannyoverton152@gmail.com>
2019-03-14 01:42:24 -0700

    Major progress: asked a question on stackoverflow for the first time, and got a response. MMore than I bargained for, i was given the code for a loop to retrive as many posts as possible easily. It takes time for the code to execute. All commands have been cleaned and have auto-self deleting. Also created a -clean function that will remove certain bot and command messages from any channel it is called from. So far so good.

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@ client.on('message', (receivedMessage) => {
     
     if (receivedMessage.content.startsWith("!")) {
         processCommand(receivedMessage)
+
     }
 })
 
@@ -27,8 +28,10 @@ function processCommand(receivedMessage) {
     console.log("Command received: " + primaryCommand)
     console.log("Arguments: " + arguments) // There may not be any arguments
 
-    if (primaryCommand == "help") {
-        helpCommand(arguments, receivedMessage)
+    
+
+    if (primaryCommand == "clean") {
+        cleanCommand(arguments, receivedMessage)
     } else if (primaryCommand == "multiply") {
         multiplyCommand(arguments, receivedMessage)
     } else if (primaryCommand == "throwback") {
@@ -38,66 +41,181 @@ function processCommand(receivedMessage) {
     }
 }
 
-function helpCommand(arguments, receivedMessage) {
-    if (arguments.length > 0) {
-        receivedMessage.channel.send("It looks like you might need help with " + arguments + ". TOO DAMN BAD GOOGLE IT.")
+
+
+
+
+function cleanCommand(arguments, receivedMessage) {
+    
+    function clean(limit = 100) {
+        return receivedMessage.channel.fetchMessages({limit}).then(async collected => {
+          let mine = collected.filter(m => m.content.startsWith("!")); // this gets only your messages
+          if (mine.size > 0) {
+            await receivedMessage.channel.bulkDelete(mine, true);
+            clean(receivedMessage.channel);
+          } else receivedMessage.channel.send("The channel is now empty!").delete(5000); // this message is deleted after 5 s
+        });
+      }
+
+    function cleanBot(limit = 100) {
+        return receivedMessage.channel.fetchMessages({limit}).then(async collected => {
+            let mine = collected.filter(m => m.author.id == "554916310212149251"); // this gets only your messages
+            if (mine.size > 0) {
+                await receivedMessage.channel.bulkDelete(mine, true);
+                clean(receivedMessage.channel);
+            } else receivedMessage.channel.send("The channel is now empty!").delete(5000); // this message is deleted after 5 s
+        });
+      }   
+
+
+    if (arguments == "bot") {
+        cleanBot();
     } else {
-        
+        clean();
     }
 }
 
 
+
+
+
+
+
+
+var bigArray;
+var testArray;
 
 function throwbackCommand(arguments, receivedMessage){
     var id = receivedMessage.member.id; // Code for getting ID of person who activates
     var user = receivedMessage.guild.members.random(); //Code for getting random user from server
     var userId = (`${user.user}`);
     var otherUserId = user.user.id
-    console.log(otherUserId)
-    console.log(userId);
+    var throwbackChannel = client.channels.get("555605025917894657")
+    
+    
+    if (receivedMessage.author.id === id) {
+        receivedMessage.delete(10000)
+    }
+
+
+    // console.log(otherUserId)
+    // console.log(userId);
+    
+    
+    async function lots_of_messages_getter(limit = 1000) {
+        sum_messages = [];
+        let last_id;
+        
+    
+        while (true) {
+            const options = { limit: 100 };
+            if (last_id) {
+                options.before = last_id;
+            }
+    
+            const messages = await receivedMessage.channel.fetchMessages(options);
+            sum_messages.push(messages.array());
+            last_id = messages.last().id;
+    
+            if (messages.size != 100 || sum_messages >= limit) {
+                break;
+            }
+        }
+        
+        //sum_messages.filter(msg => msg.author.id === id);
+        let rArray = sum_messages[Math.floor(Math.random() * sum_messages.length)]
+        //let big_array_user = big_array.filter(msg => msg.author.id === id)
+        let rMessage = rArray[Math.floor(Math.random() * rArray.length)]
+        let rUser = rMessage.author.username;
+        let rContent = rMessage.content;
+        let rChannel = rMessage.channel.name;
+        let rMessageTime = new Date(rMessage.createdTimestamp).toDateString()
+        
+        console.log(rContent)
+        console.log(rMessage)
+
+        console.log(sum_messages.length)
+        console.log(rArray.length)
+        
+
+        throwbackChannel.send({embed: {
+            color: 3447003,
+            author: {
+                name: "Throwback of " + rUser,
+                icon_url: rMessage.author.avatarURL
+            },
+            
+            title: "Throwback to when " + rUser + " sent this on " + rMessageTime + " in (" + rChannel + ")",
+            
+            //title: rUser + " sent this: " + rMessageTime + " in (" + rChannel + ")",
+            //description: rContent,
+            timestamp: new Date().toDateString(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "This throwback was requested by " + receivedMessage.author.username + " at "
+            }
+          }
+        });
+        throwbackChannel.send("-------------------------THROWBACK BELOW-------------------------")
+        throwbackChannel.send(rContent)
+        return sum_messages;
+    }
+
     
 
 
-    async function messagesBigList(){
+
+
+    async function messagesBigList() {
         
-        
-        receivedMessage.channel.fetchMessages({limit: 100}).then(msg => {
+
+        receivedMessage.channel.fetchMessages({ limit: 100 }).then(msg => {
             let toBeArray = msg;
             let firstLastPost = (toBeArray.last().id)
-            receivedMessage.channel.fetchMessages({limit: 100, before: firstLastPost }).then(msg => {
+            receivedMessage.channel.fetchMessages({ limit: 100, before: firstLastPost }).then(msg => {
                 let secondToBeArray = msg;
                 let secondLastPost = secondToBeArray.last().id;
-                receivedMessage.channel.fetchMessages({limit: 100, before: secondLastPost }).then(msg => {
+                receivedMessage.channel.fetchMessages({ limit: 100, before: secondLastPost }).then(msg => {
                     let thirdArray = msg;
                     let thirdLastPost = thirdArray.last().id;
-                    receivedMessage.channel.fetchMessages({limit: 100, before: thirdLastPost }).then(msg => {
+                    receivedMessage.channel.fetchMessages({ limit: 100, before: thirdLastPost }).then(msg => {
                         let fourthArray = msg;
                         fetchedPerson = msg.filter(message => message.author.id === id).array()
                         randMessage = fetchedPerson[Math.floor(Math.random() * fetchedPerson.length)]
                         randMessageContent = randMessage.content;
                         randMessageTime = new Date(randMessage.createdTimestamp).toDateString()
                         console.log(randMessageContent)
-                        console.log(fourthArray.last().createdTimestamp)
-                        console.log(fourthArray.last().author.username)
-                        
-                        
+                        console.log(randMessageTime)
+                        console.log(randMessage.author.username)
 
-                        receivedMessage.channel.send(randMessage.author.username + " sent this message on: " + randMessageTime + "\n" + randMessageContent)
+
+                        receivedMessage.channel.send("Give it like 30 seconds then check the `throwback` channel for the response")
+                        throwbackChannel.send(randMessage.author.username + " sent this message on: " + randMessageTime + "\n" + randMessageContent)
                     })
-                    
+
                 })
-                
-                
+
+
             })
 
-            
-                
-        })
+
+
+        }).catch((err) => receivedMessage.channel.send(user.user.username + " is a bitch and didn't send any messages recently, sorry!"))
 
         //receivedMessage.channel.fetchMessages({limit: 100, before: firstArray.last().message.id }) 
         //console.log(secondArray.last().message.id)
-          
+
     }
+
+
+    
+
+    function getBigArray() {
+        console.log(bigArray.filter(message => message.author.id === id).array())
+        
+    }
+
+
 
 
     if (arguments.length > 10 ) {
@@ -115,48 +233,25 @@ function throwbackCommand(arguments, receivedMessage){
 
     } else if (arguments == "me") {
         messagesBigList();
+        //getBigArray();
         
         
 
-    } else if (arguments == "me2") {
-        var x = 0;
-        var toBeArray;
+    } else if (arguments == "test") {
+        console.log(sum_messages)
         
 
-        while (x < 5) {
-            var i;
-            receivedMessage.channel.fetchMessages({ limit: 100, before: i }).then(msg => {
-                toBeArray = msg;
-                i = toBeArray.last().id;
-                messageLoop(i);
 
-            })
+    } else if (arguments == "random") {
+        receivedMessage.channel.send("Give it like 30 seconds then check the `throwback` channel for the response")
+        lots_of_messages_getter();
+        
 
-            x++;
-            console.log(toBeArray);
-        } 
-
-        function messageLoop(data){
-            console.log(data)
-        }
+        
     } else {
+        receivedMessage.channel.send("Give it like 30 seconds then check the `throwback` channel for the response").then(msg => msg.delete(10000))
+        lots_of_messages_getter();
         
-
-    
-
-    receivedMessage.channel.fetchMessages({limit: 100}).then(message => {
-        const fetched = message.filter(msg => msg.author.id === otherUserId)
-        //console.log(fetched)
-        fetchedArray = fetched.array();
-        randomMessage = fetchedArray[Math.floor(Math.random() * fetchedArray.length)]
-        const finalMessageContent = (randomMessage.content)
-        const finalMessageTime = new Date(randomMessage.createdTimestamp).toDateString()
-
-        receivedMessage.channel.send(user.user.username + " sent this message on: " + finalMessageTime + "\n" + finalMessageContent)
-        
-    })
-    .catch((err) => receivedMessage.channel.send(user.user.username + " is a bitch and didn't send any messages recently, sorry!"))
-
 
     }
 
